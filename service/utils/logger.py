9ba37ad778785c2960d14629fc6d6af736e9952d
sakuya9t <sakuya9t@vip.qq.com>
2019-10-03 19:19:29 +1000

    update logger so that can write to file or not using colors.

--- service/utils/logger.py
+++ service/utils/logger.py
@@ -1,19 +1,37 @@
 import datetime
 
+from constant import CONFIG_PATH
+from similarity.Config import Config
+
+INFO = 0
+WARNING = 1
+ERROR = 2
+
+enable_color = Config(CONFIG_PATH).get('logger/color-enabled')
+write_to_file = Config(CONFIG_PATH).get('logger/write-to-file')
+log_file_path = Config(CONFIG_PATH).get('logger/logfile-path')
+
 
 def info(message):
-    time = str(datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]"))
-    message = str(message)
-    print("\033[0m (INFO) {time} {message}".format(time=time, message=message))
+    __print_message(message, INFO)
 
 
 def error(message):
-    time = str(datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]"))
-    message = str(message)
-    print("\033[91m (ERROR) {time} {message}".format(time=time, message=message))
+    __print_message(message, ERROR)
 
 
 def warning(message):
+    __print_message(message, WARNING)
+
+
+def __print_message(message, level):
+    color = {INFO: '\033[0m', WARNING: '\033[93m ', ERROR: '\033[91m '}
+    prefix = {INFO: '(INFO)', WARNING: '(WARNING)', ERROR: '(ERROR)'}
     time = str(datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]"))
-    message = str(message)
-    print("\033[93m (WARNING) {time} {message}".format(time=time, message=message))
+    log_body = '{prefix} {time} {message}'.format(prefix=prefix[level], time=time, message=message)
+    if write_to_file:
+        with open(log_file_path, 'a') as log_file:
+            log_file.write(log_body + '\n')
+    elif enable_color:
+        log_body = '{} {}'.format(color[level], log_body)
+        print(log_body)
