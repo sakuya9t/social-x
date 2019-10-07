de45bfe49a360e2c8b1dfe209a0470fb72b74948
sakuya9t <sakuya9t@vip.qq.com>
2019-10-07 17:42:31 +1100

    generate model file and stats files in different cases

--- service/Tests/automation/ModelUpgradeTests.py
+++ service/Tests/automation/ModelUpgradeTests.py
@@ -23,32 +23,34 @@ class ModelUpgradeTests(unittest.TestCase):
         model = generate_model(REALTIME_MODE, cross_features=True)
 
     def test_export_model_realtime(self):
-        model = generate_model(REALTIME_MODE, cross_features=True)
-        export_model(model, REALTIME_MODE)
+        enable_crossfeature = True
+        model = generate_model(REALTIME_MODE, cross_features=enable_crossfeature)
+        export_model(model, REALTIME_MODE, enable_crossfeature)
         date_str = date.today().strftime('%y%m%d')
         jsonfile_path = MODEL_FILE_BASE_PATH + "model_realtime{}.json".format(date_str)
         h5file_path = MODEL_FILE_BASE_PATH + "model_realtime{}.h5".format(date_str)
         self.assertTrue(os.path.exists(jsonfile_path) and os.path.exists(h5file_path))
 
     def test_export_model_batch(self):
-        model = generate_model(BATCH_MODE, cross_features=True)
-        export_model(model, BATCH_MODE)
+        enable_crossfeature = True
+        model = generate_model(BATCH_MODE, cross_features=enable_crossfeature)
+        export_model(model, BATCH_MODE, enable_crossfeature)
         date_str = date.today().strftime('%y%m%d')
         jsonfile_path = MODEL_FILE_BASE_PATH + "model_batch{}.json".format(date_str)
         h5file_path = MODEL_FILE_BASE_PATH + "model_batch{}.h5".format(date_str)
         self.assertTrue(os.path.exists(jsonfile_path) and os.path.exists(h5file_path))
 
     def test_import_model(self):
-        model = import_model('model_batch191006')
+        model = import_model('model_batch191007')
         self.assertTrue(isinstance(model, Sequential))
-        model = import_model('model_realtime191006')
+        model = import_model('model_realtime191007')
         self.assertTrue(isinstance(model, Sequential))
 
     def test_batch(self):
         upgrade_model_batch()
 
     def test_import_stats(self):
-        filename = MODEL_FILE_BASE_PATH + "stats191006.json"
+        filename = MODEL_FILE_BASE_PATH + "stats191007.json"
         stats = import_stats(filename)
         self.assertTrue(isinstance(stats, pd.DataFrame))
 
--- service/automation/batch/ModelUpgrade.py
+++ service/automation/batch/ModelUpgrade.py
@@ -12,6 +12,8 @@ from constant import REALTIME_MODE, BATCH_MODE, DATABASE_LABELED_DATA, MODEL_FIL
 from utils.Couch import Couch
 
 EPOCHS = 1000
+FEATURE_COUNT_REALTIME = 3
+FEATURE_COUNT_BATCH = 6
 
 
 def norm(x, train_stats):
@@ -33,14 +35,12 @@ def build_model(train_dataset):
     return _model
 
 
-def generate_model(mode, cross_features=False):
-    logger.info('Start generating model in {} mode.'.format('REALTIME' if mode == REALTIME_MODE else 'BATCH'))
-    logger.info('Production of features {}.'.format('enabled' if cross_features else 'disabled'))
-    items = Couch(DATABASE_LABELED_DATA).query_all()
-    items = list(filter(lambda x: 'label' in x['vector'].keys(), items))
-    logger.info('Retrieved {} labelled data from the database.'.format(len(items)))
-    l = [[list([float(a) for a in x.values()]) if isinstance(x, dict) else [float(x)] for x in item['vector'].values()]
-         for item in items]
+def __unpack_feature_dict(item):
+    return [list([float(a) for a in x.values()]) if isinstance(x, dict) else [float(x)] for x in item['vector'].values()]
+
+
+def generate_feature_vectors(items, mode, cross_features):
+    l = [__unpack_feature_dict(item) for item in items]
     if mode == REALTIME_MODE:
         l = [x[0:3] + x[-1:] for x in l]
     else:
@@ -49,15 +49,17 @@ def generate_model(mode, cross_features=False):
 
     if cross_features:
         if mode == REALTIME_MODE:
-            l = [np.append(np.array(item[:-1]).dot(np.array(item[:-1]).reshape((1, 3))).flatten(), item[-1]) for item in
-                 l]
+            l = [np.append(np.array(item[:-1]).dot(np.array(item[:-1]).reshape((1, FEATURE_COUNT_REALTIME))).flatten(),
+                           item[-1]) for item in l]
         else:
-            l = [np.append(np.array(item[:-1]).dot(np.array(item[:-1]).reshape((1, 6))).flatten(), item[-1]) for item in
-                 l]
+            l = [np.append(np.array(item[:-1]).dot(np.array(item[:-1]).reshape((1, FEATURE_COUNT_BATCH))).flatten(),
+                           item[-1]) for item in l]
     else:
         l = [np.array(x).flatten() for x in l]
+    return l
 
-    data_dict = {'col{}'.format(x): l[x] for x in range(len(l))}
+
+def __get_column_names(mode, cross_features):
     if mode == REALTIME_MODE:
         if cross_features:
             column_names = ['feature{}'.format(i) for i in range(0, 9)]
@@ -70,16 +72,33 @@ def generate_model(mode, cross_features=False):
             column_names.append('label')
         else:
             column_names = ['username', 'profileImage', 'self_desc', 'readability', 'post_text', 'uclassify', 'label']
+    return column_names
+
+
+def generate_dataset(data_list, mode, cross_features):
+    data_dict = {'col{}'.format(x): data_list[x] for x in range(len(data_list))}
+    column_names = __get_column_names(mode, cross_features)
 
     raw_dataset = pd.DataFrame.from_dict(data_dict, orient='index', columns=column_names)
-    dataset = raw_dataset.copy()
+    return raw_dataset.copy()
+
+
+def generate_model(mode, cross_features=False):
+    logger.info('Start generating model in {} mode.'.format('REALTIME' if mode == REALTIME_MODE else 'BATCH'))
+    logger.info('Production of features {}.'.format('enabled' if cross_features else 'disabled'))
+    items = Couch(DATABASE_LABELED_DATA).query_all()
+    items = list(filter(lambda x: 'label' in x['vector'].keys(), items))
+    logger.info('Retrieved {} labelled data from the database.'.format(len(items)))
+    l = generate_feature_vectors(items, mode, cross_features)
+
+    dataset = generate_dataset(l, mode, cross_features)
     train_dataset = dataset.sample(frac=0.8, random_state=0)
     test_dataset = dataset.drop(train_dataset.index)
 
     train_stats = train_dataset.describe()
     train_stats.pop("label")
     train_stats = train_stats.transpose()
-    export_stats(train_stats)
+    export_stats(train_stats, mode, cross_features)
     logger.info('Exported training stats.')
 
     train_labels = train_dataset.pop('label')
@@ -115,11 +134,11 @@ def generate_model(mode, cross_features=False):
     return model
 
 
-def export_stats(stats):
+def export_stats(stats, mode, enable_crossfeature):
     json_data = stats.to_json()
-    date_str = date.today().strftime('%y%m%d')
-    filename = MODEL_FILE_BASE_PATH + "stats{}.json".format(date_str)
-    Config(ALGOCONFIG_PATH).set('model-name/train-stats', "stats{}.json".format(date_str))
+    stat_name = _generate_stat_name(mode, enable_crossfeature)
+    filename = MODEL_FILE_BASE_PATH + "{}.json".format(stat_name)
+    Config(ALGOCONFIG_PATH).set('model-name/train-stats', "{}.json".format(stat_name))
     with open(filename, "w") as json_file:
         json_file.write(json_data)
 
@@ -138,14 +157,21 @@ class PrintDot(keras.callbacks.Callback):
         print('.', end='')
 
 
-def _generate_model_name(mode):
+def _generate_model_name(mode, enable_crossfeature):
     date_str = date.today().strftime('%y%m%d')
-    return "model_{}{}".format('realtime' if mode == REALTIME_MODE else 'batch', date_str)
+    return "model_{}{}{}".format('realtime' if mode == REALTIME_MODE else 'batch', date_str,
+                                 "cross" if enable_crossfeature else "")
 
 
-def export_model(model, mode):
+def _generate_stat_name(mode, enable_crossfeature):
+    date_str = date.today().strftime('%y%m%d')
+    return "stat_{}{}{}".format('realtime' if mode == REALTIME_MODE else 'batch', date_str,
+                                "cross" if enable_crossfeature else "")
+
+
+def export_model(model, mode, enable_crossfeature):
     model_json = model.to_json(indent=2)
-    model_name = _generate_model_name(mode)
+    model_name = _generate_model_name(mode, enable_crossfeature)
     jsonfile_path = MODEL_FILE_BASE_PATH + model_name + ".json"
     h5file_path = MODEL_FILE_BASE_PATH + model_name + ".h5"
     with open(jsonfile_path, "w") as json_file:
@@ -174,10 +200,10 @@ def import_model(model_name):
 def upgrade_model_batch():
     enable_crossfeature = Config(ALGOCONFIG_PATH).get('enable-cross-feature')
     _model = generate_model(REALTIME_MODE, cross_features=enable_crossfeature)
-    model_name = export_model(_model, REALTIME_MODE)
+    model_name = export_model(_model, REALTIME_MODE, enable_crossfeature)
     Config(ALGOCONFIG_PATH).set('model-name/realtime', model_name)
     _model = generate_model(BATCH_MODE, cross_features=enable_crossfeature)
-    model_name = export_model(_model, BATCH_MODE)
+    model_name = export_model(_model, BATCH_MODE, enable_crossfeature)
     Config(ALGOCONFIG_PATH).set('model-name/batch', model_name)
 
 
