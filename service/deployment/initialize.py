7c60702adbfd6bc0022f8cf3d4037fda82a6a1d0
sakuya9t <sakuya9t@vip.qq.com>
2019-10-17 09:47:14 +1100

    update key files path

--- service/deployment/initialize.py
+++ service/deployment/initialize.py
@@ -1,7 +1,9 @@
 from constant import ROOT_DIR
 from similarity import TextUtils
+from utils import Decryptor
 
 config_template_path = ROOT_DIR + 'config/config-template.json'
 config_deploy_template_path = ROOT_DIR + 'config/config-deploy-template.json'
+Decryptor.generate_key()
 TextUtils.initialize()
 
