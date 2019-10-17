f750841ae2ee61a4f98fafea5d4c65e2889af1c8
sakuya9t <sakuya9t@vip.qq.com>
2019-10-17 14:14:41 +1100

    add all platform integrated test

---
+++ service/Tests/similarity/AllPlatformSimilarityTests.py
@@ -0,0 +1,26 @@
+import unittest
+
+from constant import REALTIME_MODE, DATABASE_DATA_AWAIT_FEEDBACK, BATCH_MODE
+from similarity.SimCalculator import SimCalculator
+from utils.QueryGenerator import retrieve
+
+
+class AllPlatformSimilarityTests(unittest.TestCase):
+    def test_twitter_instagram(self):
+        handler = SimCalculator()
+        account1 = {'platform': 'twitter', 'account': '1angharad_rees'}
+        account2 = {'platform': 'instagram', 'account': 'kaligraphicprint'}
+        info1 = retrieve(account1, BATCH_MODE)
+        info2 = retrieve(account2, BATCH_MODE)
+        info1['platform'] = account1['platform'].lower()
+        info2['platform'] = account2['platform'].lower()
+        vector = handler.calc(info1, info2, enable_networking=False, mode=BATCH_MODE)
+        doc_id = handler.store_result(info1, info2, vector, DATABASE_DATA_AWAIT_FEEDBACK)
+        self.assertIsNotNone(doc_id)
+
+    def test_twitter_pinterest(self):
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
