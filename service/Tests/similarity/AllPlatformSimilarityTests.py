9477f2f0d22ada24605abfe55c9410d84ec1f073
sakuya9t <sakuya9t@vip.qq.com>
2019-10-18 18:39:54 +1100

    add integration tests: twi-twi, twi-pin, ins-fli

--- service/Tests/similarity/AllPlatformSimilarityTests.py
+++ service/Tests/similarity/AllPlatformSimilarityTests.py
@@ -18,8 +18,41 @@ class AllPlatformSimilarityTests(unittest.TestCase):
         doc_id = handler.store_result(info1, info2, vector, DATABASE_DATA_AWAIT_FEEDBACK)
         self.assertIsNotNone(doc_id)
 
+    def test_twitter_twitter(self):
+        handler = SimCalculator()
+        account1 = {'platform': 'twitter', 'account': '1angharad_rees'}
+        account2 = {'platform': 'twitter', 'account': 'ClassicVines1'}
+        info1 = retrieve(account1, BATCH_MODE)
+        info2 = retrieve(account2, BATCH_MODE)
+        info1['platform'] = account1['platform'].lower()
+        info2['platform'] = account2['platform'].lower()
+        vector = handler.calc(info1, info2, enable_networking=False, mode=BATCH_MODE)
+        doc_id = handler.store_result(info1, info2, vector, DATABASE_DATA_AWAIT_FEEDBACK)
+        self.assertIsNotNone(doc_id)
+
     def test_twitter_pinterest(self):
-        self.assertTrue(False)
+        handler = SimCalculator()
+        account1 = {'platform': 'twitter', 'account': 'ClassicVines1'}
+        account2 = {'platform': 'pinterest', 'account': 'mariesbazaar'}
+        info1 = retrieve(account1, BATCH_MODE)
+        info2 = retrieve(account2, BATCH_MODE)
+        info1['platform'] = account1['platform'].lower()
+        info2['platform'] = account2['platform'].lower()
+        vector = handler.calc(info1, info2, enable_networking=False, mode=BATCH_MODE)
+        doc_id = handler.store_result(info1, info2, vector, DATABASE_DATA_AWAIT_FEEDBACK)
+        self.assertIsNotNone(doc_id)
+
+    def test_instagram_flickr(self):
+        handler = SimCalculator()
+        account1 = {'platform': 'instagram', 'account': 'photographynadia'}
+        account2 = {'platform': 'flickr', 'account': 'nadiaphotograhy'}
+        info1 = retrieve(account1, BATCH_MODE)
+        info2 = retrieve(account2, BATCH_MODE)
+        info1['platform'] = account1['platform'].lower()
+        info2['platform'] = account2['platform'].lower()
+        vector = handler.calc(info1, info2, enable_networking=False, mode=BATCH_MODE)
+        doc_id = handler.store_result(info1, info2, vector, DATABASE_DATA_AWAIT_FEEDBACK)
+        self.assertIsNotNone(doc_id)
 
 
 if __name__ == '__main__':
